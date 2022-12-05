efd61d7eb49c5d5a290d493e676b7f6a58519644
admillad <admillad@hotmail.com>
2022-12-05 20:58:50 +0100

    my first commit

---
+++ src/main/java/se/jensen/javacourse/week3/MatildaAdeleng/MatildaAdelengApplication.java
@@ -0,0 +1,10 @@
+package se.jensen.javacourse.week3.MatildaAdeleng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatildaAdelengApplication {
+
+
+}
---
+++ src/main/java/se/jensen/javacourse/week3/MatildaAdeleng/main.java
@@ -0,0 +1,9 @@
+package se.jensen.javacourse.week3.MatildaAdeleng;
+
+import org.springframework.boot.SpringApplication;
+
+public class main {
+    public static void main(String[] args) {
+        SpringApplication.run(MatildaAdelengApplication.class, args);
+    }
+}
