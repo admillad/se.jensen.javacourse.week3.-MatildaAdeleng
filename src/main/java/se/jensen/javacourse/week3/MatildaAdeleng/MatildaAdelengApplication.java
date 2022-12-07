e3081f4f84aa231d01f271bd9263a225f0f368ab
admillad <admillad@hotmail.com>
2022-12-07 21:42:02 +0100

    trying another change

--- src/main/java/se/jensen/javacourse/week3/MatildaAdeleng/MatildaAdelengApplication.java
+++ src/main/java/se/jensen/javacourse/week3/MatildaAdeleng/MatildaAdelengApplication.java
@@ -1,16 +1,34 @@
 package se.jensen.javacourse.week3.MatildaAdeleng;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class MatildaAdelengApplication {
     public static void main(String[] args) {
         SpringApplication.run(MatildaAdelengApplication.class, args);
-
-
     }
+
+    @GetMapping("/author/name")
+    public static String firstName() {
+        return "Matilda";
+    }
+
+    @GetMapping("/author")
+    public Person getPerson(HttpServletRequest req,
+                            @RequestBody Person reqBody) {
+        Person matilda = new Person("Matilda", "Adeleng", 26);
+        return reqBody;
+    }
+
+    @GetMapping("/square-number/{nr}")
+    public double squared(@PathVariable double input) {
+        double square = input * input;
+        return square;
+    }
+    //return a 400 status with response entity (body: , httpStatus.BAD_Request)
 
 }
--- src/main/java/se/jensen/javacourse/week3/MatildaAdeleng/Person.java
+++ src/main/java/se/jensen/javacourse/week3/MatildaAdeleng/Person.java
@@ -5,6 +5,30 @@ public class Person {
     String lastname;
     int age;
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public Person(String firstname, String lastname, int age) {
         this.firstname = firstname;
         this.lastname = lastname;
