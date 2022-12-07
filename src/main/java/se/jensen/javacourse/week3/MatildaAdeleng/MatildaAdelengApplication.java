75b99702ddd5afbefb45a10bf05a2dc3a0c06a3d
admillad <admillad@hotmail.com>
2022-12-08 00:06:43 +0100

    my final commit

--- src/main/java/se/jensen/javacourse/week3/MatildaAdeleng/MatildaAdelengApplication.java
+++ src/main/java/se/jensen/javacourse/week3/MatildaAdeleng/MatildaAdelengApplication.java
@@ -1,9 +1,10 @@
 package se.jensen.javacourse.week3.MatildaAdeleng;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @SpringBootApplication
 @RestController
@@ -12,23 +13,20 @@ public class MatildaAdelengApplication {
         SpringApplication.run(MatildaAdelengApplication.class, args);
     }
 
-    @GetMapping("/author/name")
-    public static String firstName() {
-        return "Matilda";
-    }
 
     @GetMapping("/author")
-    public Person getPerson(HttpServletRequest req,
-                            @RequestBody Person reqBody) {
-        Person matilda = new Person("Matilda", "Adeleng", 26);
-        return reqBody;
+    public Object JsonPerson() {
+        return new Person("Matilda", "Adeleng", 26);
     }
 
     @GetMapping("/square-number/{nr}")
-    public double squared(@PathVariable double input) {
-        double square = input * input;
-        return square;
+    public String squareNumber(@PathVariable("nr") String strNr) {
+        double dobNr;
+        try {
+            dobNr = Double.parseDouble(strNr);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "enter a number");
+        }
+        return "" + dobNr * dobNr;
     }
-    //return a 400 status with response entity (body: , httpStatus.BAD_Request)
-
 }
